4d46f8ee71d4ed7e176167f1243c4448fae7eebb
ArmorDarks <arkahor@gmail.com>
2015-10-29 13:01:08 +0200

    [js][misc] import css with loader too

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -8,12 +8,12 @@ import './fancybox/source/helpers/jquery.fancybox-buttons'
 import './fancybox/source/helpers/jquery.fancybox-media.js'
 import './fancybox/source/helpers/jquery.fancybox-thumbs.js'
 
-import './tipsy/jquery.tipsy'
+import './fancybox/source/jquery.fancybox.css!'
+import './fancybox/source/helpers/jquery.fancybox-buttons.css!'
+import './fancybox/source/helpers/jquery.fancybox-thumbs.css!'
 
-// import './tipsy/tipsy.css!'
-// import './fancybox/source/jquery.fancybox.css!'
-// import './fancybox/source/helpers/jquery.fancybox-buttons.css!'
-// import './fancybox/source/helpers/jquery.fancybox-thumbs.css!'
+import './tipsy/jquery.tipsy'
+import './tipsy/tipsy.css!'
 
 $(() => {
   console.log('jQuery version is: ' + $().jquery)
@@ -23,22 +23,25 @@ $(() => {
   $('.divslide').showHide({
     speed: 500,  // speed you want the toggle to happen
     easing: '',  // the animation effect you want. Remove this line if you dont want an effect and if you haven't included jQuery UI
-    changeText: 0, // if you dont want the button text to change, set this to 0
+    changeText: 0 // if you dont want the button text to change, set this to 0
   })
 
    $('.divslide2').showHide({
     speed: 500,  // speed you want the toggle to happen
     easing: '',  // the animation effect you want. Remove this line if you dont want an effect and if you haven't included jQuery UI
-    changeText: 0, // if you dont want the button text to change, set this to 0
+    changeText: 0 // if you dont want the button text to change, set this to 0
   })
 
    $('.divslide3').showHide({
     speed: 500,  // speed you want the toggle to happen
     easing: '',  // the animation effect you want. Remove this line if you dont want an effect and if you haven't included jQuery UI
-    changeText: 0, // if you dont want the button text to change, set this to 0
-  });
+    changeText: 0 // if you dont want the button text to change, set this to 0
+  })
 
-  $('.shrink_work').tipsy({gravity:'s', live: true})
+  $('.shrink_work').tipsy({
+    gravity: 's',
+    live: true
+  })
 
   $(".fancybox").fancybox()
 })
